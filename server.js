79f436bbc2656167a7d8e673d014dcdde89b5c75
RobertMisch <robertmisch94@gmail.com>
2020-05-06 18:37:51 -0400

    getting started

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+//custom middleware just making a change for lambda forms
 
 function logger(req, res, next) {}
 
